244cc21d49a43c4688e06bd0cb212bfcd5267e7c
Pranathi P K <pranathikumaran@gmail.com>
2026-06-22 13:59:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
      const [msg,setmsg]=useState("");
     const handlemessage=()=>{
-    fetch("https://railway.app")
+    fetch("https://backend-production-dbea.up.railway.app/hello")
     .then(res=>res.text())
     .then((m)=>setmsg(m));}
    
